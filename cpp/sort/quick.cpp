b6e79e42dca738ba8343bf70f54903dc2f519f48
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2018-04-05 08:16:25 +0530

    Quick Sort

--- cpp/sort/quick.cpp
+++ cpp/sort/quick.cpp
@@ -5,15 +5,28 @@ using namespace std;
 void show(int a[], int size) {
 	for(int i = 0; i < size; i++)
 		cout << a[i] << "\t";
+	cout << endl;
 }
 
 void swap(int *a, int *b) {
-	*a = *a + *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 int partition(int a[], int low, int high) {
+
+	int pivot = a[high];
+
+	int i = low - 1;
+
+	for(int j = low; j <= high - 1; j++)
+		if(a[j] <= pivot)
+			swap(&a[++i], &a[j]);
+
+	swap(&a[i+1], &a[high]);
+
+	return i+1;
 }
 
 void quickSort(int a[], int low, int high) {
@@ -30,12 +43,11 @@ void quickSort(int a[], int low, int high) {
 
 int main() {
 
-	int a[10] = {4, 9, 7, 8, 1, 3, 0, 2, 5, 6};
+	int a[10] = {7, 9, 2, 4, 8, 0, 1, 6, 3, 5};
 
 	quickSort(a, 0, 9);
 
-	show(a, 0 , 9);
-	cout << endl;
+	show(a, 10);
 
 	return 0;
 }
