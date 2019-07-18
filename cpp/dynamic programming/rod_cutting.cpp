be24e27138f27c543ea7c85cf24794d195417b80
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2019-07-18 13:21:23 +0530

    Updated Rod Cutting

--- cpp/dynamic programming/rod_cutting.cpp
+++ cpp/dynamic programming/rod_cutting.cpp
@@ -2,9 +2,10 @@
 
 The rod-cutting problem is the following:
 
-Given a rod of length n inches and a table of prices pi for i = 1, 2,.....,n, determine the maximum revenue rn obtainable 
-by cutting up the rod and selling the pieces. 
-*/
+Given a rod of length n inches and a table of prices pi for i = 1, 2,.....,n, determine the maximum 
+revenue rn obtainable by cutting up the rod and selling the pieces. 
+
+// CLRS Solution
 
 #include <bits/stdc++.h>
 
@@ -37,4 +38,61 @@ int cutRod(int n) {
 int main() {
 	int n = 1;
 	cout << "\nRod Size: " << n << "\nMax Price: " << cutRod(n);
+}
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Brute-force
+int profit_bf(vector<int>& lengths, vector<int>& prices, int length, int index) {
+    if(index >= lengths.size()) {
+        return 0;
+    }
+    
+    int profit1 = 0;
+    
+    if(lengths[index] <= length) {
+        profit1 = prices[index] + profit_bf(lengths, prices, length-lengths[index], index);
+    }
+    
+    int profit2 = profit_bf(lengths, prices, length, index+1);
+    
+    return max(profit1, profit2);
+}
+
+vector<vector<int>> memo;
+
+// Top-down
+int profit_td(vector<int>& lengths, vector<int>& prices, int length, int index) {
+    if(index >= lengths.size()) {
+        return 0;
+    }
+    
+    if(memo[index][length] != -1) {
+        return memo[index][length];
+    }
+    
+    int profit1 = 0;
+    
+    if(lengths[index] <= length) {
+        profit1 = prices[index] + profit_td(lengths, prices, length-lengths[index], index);
+    }
+    
+    int profit2 = profit_td(lengths, prices, length, index+1);
+    
+    return (memo[index][length] = max(profit1, profit2));
+}
+
+int main() {
+    vector<int> lengths = {1, 2, 3, 4, 5};
+    
+    vector<int> prices = {2, 6, 7, 10, 13};
+    
+    int length = 5;
+    
+    memo.resize(prices.size(), vector<int>(length+1, -1));
+    
+    cout << "Profit: " << profit_td(lengths, prices, length, 0);
 }
