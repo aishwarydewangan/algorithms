7632a7b8a058c2bcafc2e4fb62bf792489cbf26a
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2019-07-24 18:41:36 +0530

    Update LCS

--- cpp/dynamic programming/longest_common_substring.cpp
+++ cpp/dynamic programming/longest_common_substring.cpp
@@ -21,6 +21,7 @@ using namespace std;
 
 vector<vector<vector<int>>> memo;
 
+// Top-down
 int lcs(string& input1, string& input2, int index1, int index2, int count) {
     if(index1 == input1.size() or index2 == input2.size()) {
         return count;
@@ -43,10 +44,29 @@ int lcs(string& input1, string& input2, int index1, int index2, int count) {
     return memo[index1][index2][count];
 }
 
+// Bottom-up
+int lcs(string& input1, string& input2) {
+    
+    int length = INT_MIN, n1 = input1.size(), n2 = input2.size();
+    
+    vector<vector<int>> dp(n1+1, vector<int>(n2+1));
+    
+    for(int i = 1; i <= n1; i++) {
+        for(int j = 1; j <= n2; j++) {
+            if(input1[i] == input2[j]) {
+                dp[i][j] = dp[i-1][j-1]+1;
+                length = max(length, dp[i][j]);
+            }
+        }
+    }
+    
+    return length;
+}
+
 int main() {
     string input1 = "abdca", input2 = "cbda";
     int n1 = input1.size(), n2 = input2.size();
     memo.resize(n1, vector<vector<int>>(n2, vector<int>(max(n1, n2), -1)));
-    cout << "Length of Longest Common Substring: " << lcs(input1, input2, 0, 0, 0);
+    cout << "Length of Longest Common Substring: " << lcs(input1, input2);
     return 0;
 }
