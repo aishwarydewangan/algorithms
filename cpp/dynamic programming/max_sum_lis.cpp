d320b0f144c823b91e06ea804ddc1a13e66a7e07
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2019-07-29 12:23:33 +0530

    Update Max Sum Increasing Subsequence

--- cpp/dynamic programming/max_sum_lis.cpp
+++ cpp/dynamic programming/max_sum_lis.cpp
@@ -23,6 +23,51 @@ using namespace std;
 
 map<string, int> mp;
 
+vector<vector<int>> memo;
+
+
+// Bottom-up
+int maxLIS(vector<int>& nums) {
+    vector<int> dp(nums);
+    
+    for(int i = 1; i < nums.size(); i++) {
+        for(int j = 0; j < i; j++) {
+            if(nums[j] < nums[i])
+                dp[i] = max(dp[i], dp[j]+nums[i]);
+        }
+    }
+    
+    int sum = INT_MIN;
+    
+    for(auto d : dp) {
+        sum = max(sum, d);
+    }
+    
+    return sum;
+}
+
+// Top-down
+int maxLIS(vector<int>& nums, int pre, int curr) {
+    if(curr == nums.size()) {
+        return 0;
+    }
+    
+    if(memo[pre+1][curr] != -1) {
+        return memo[pre+1][curr];
+    }
+    
+    int s1 = 0;
+    
+    if(pre == -1 or nums[curr] > nums[pre]) {
+        s1 = lcs(nums, curr, curr+1)+nums[curr];
+    }
+    
+    int s2 = lcs(nums, pre, curr+1);
+    
+    return (memo[pre+1][curr] = max(s1, s2));
+}
+
+// Top-down
 int maxLIS(vector<int>& nums, int pre, int curr, int sum) {
     if(curr == nums.size()) {
         return sum;
@@ -47,6 +92,8 @@ int maxLIS(vector<int>& nums, int pre, int curr, int sum) {
 
 int main() {
     vector<int> nums = {-4,10,3,7,15};
+    int n = nums.size();
+    memo.resize(n+1, vector<int>(n+1, -1));
     cout << "Maximum Sum of Increasing Subsequence: " << maxLIS(nums, -1, 0, 0);
     return 0;
 }
