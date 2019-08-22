31aa155a603494f44703eaac7b59aa6763b4aa22
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2019-08-22 11:48:12 +0530

    Added Largest Sum Continuous Subarray

--- README.md
+++ README.md
@@ -70,3 +70,4 @@
 
 * Misc
 	* Nth-Fibonacci using Matrix Exponentiation
+	* Largest Sum Continuous Subarray (Kadane's Algorithm)
---
+++ cpp/misc/largest_sum_continuous_subarray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int process(vector<int>& nums) {
+	int max_so_far = nums[0], curr_max = nums[0];
+	for(int i = 1; i < nums.size(); i++) {
+		curr_max = max(curr_max, curr_max+nums[i]);
+		max_so_far = max(max_so_far, curr_max);
+	}
+	return max_so_far;
+}
+
+int main() {
+	vector<int> nums = {-2, -3, 4, -1, -2, 1, 5, -3};
+	cout << process(nums);
+	return 0;
+}
