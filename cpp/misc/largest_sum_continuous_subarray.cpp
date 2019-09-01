768e5055fce28ceceae50b5e669c3ef4beccb20b
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2019-09-01 16:55:25 +0530

    Update Solution

--- cpp/misc/largest_sum_continuous_subarray.cpp
+++ cpp/misc/largest_sum_continuous_subarray.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int process(vector<int>& nums) {
 	int max_so_far = nums[0], curr_max = nums[0];
 	for(int i = 1; i < nums.size(); i++) {
-		curr_max = max(curr_max, curr_max+nums[i]);
+		curr_max = max(nums[i], curr_max+nums[i]);
 		max_so_far = max(max_so_far, curr_max);
 	}
 	return max_so_far;
