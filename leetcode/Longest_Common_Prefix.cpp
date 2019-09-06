01ad83a10f71e82020c542f2fbbcb0992b6f0e87
Aishwary Dewangan <aishwary.dewangan@gmail.com>
2019-09-06 22:31:03 +0530

    Updated Solutions

--- leetcode/Longest_Common_Prefix.cpp
+++ leetcode/Longest_Common_Prefix.cpp
@@ -8,24 +8,48 @@ Example 1:
 
 Input: ["flower","flow","flight"]
 Output: "fl"
+
 Example 2:
 
 Input: ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
-Note:
 
-All given inputs are in lowercase letters a-z.
 */
 
+/*
+
+Solution 1: 
+
+    Intuition: LCP(S1, S2, S3, ......, Sn) = LCP(LCP(LCP(S1, S2), S3), ...., Sn)
+
+    Time Complexity: O(S), S = sum of all characters in all strings
+
+    Space Complexity: O(1)
+
+Solution 2: Vertical Scanning
+
+    Thought of this solution.
+
+    Intuition: Starting from index 0 scan each index of all strings till minLength (length of shortest string)
+
+    Time Complexity: Let n = no. of strings
+        Worst Case: All strings have same length m, then = O(m.n)
+        Best Case: O(n.minLength)
+
+    Space Complexity: O(1)
+
+Solution 3: Divide and Conquer
+
+    Intuition: LCP(S1, S2, S3, ......, Sn) = LCP(LCP(S1, S2, ...., Sk), LCP(Sk+1, Sk+2, ....., Sn))
+*/
+
+//My Solution: Vertical Scanning
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if(strs.size() == 0) {
-            return "";
-        }
-        if(strs.size() == 1) {
-            return strs[0];
+        if(strs.size() == 0 || strs.size() == 1) {
+            return (strs.size() ? strs[0] : "");
         }
         string ans = "";
         int i = 0;
@@ -39,8 +63,9 @@ public:
                     if(i > strs[j].size()) {
                         loop = false;
                     } else {
-                        if(first[i] != strs[j][i])
+                        if(first[i] != strs[j][i]) {
                             loop = false;
+                        }
                     }
                 }
                 if(loop) {
